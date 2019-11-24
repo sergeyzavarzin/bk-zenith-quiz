d54245b2bcc8043d23a0c80e9eaa674d7bbe7d39
Sergey <tweek4000@yandex.ru>
2019-11-24 23:16:02 +0400

    Feat: slides

--- src/Panels/Welcome/slides.js
+++ src/Panels/Welcome/slides.js
@@ -7,12 +7,12 @@ import SlideMobile4 from './images/mobile/slide-table.svg';
 import SlideMobile5 from './images/mobile/slide-team.svg';
 import SlideMobile6 from './images/mobile/slide-final.svg';
 
-import SlideDesktop1 from './images/desktop/screen-matches.svg';
-import SlideDesktop2 from './images/desktop/screen-voting.svg';
-import SlideDesktop3 from './images/desktop/screen-score.svg';
-import SlideDesktop4 from './images/desktop/screen-table.svg';
-import SlideDesktop5 from './images/desktop/screen-team.svg';
-import SlideDesktop6 from './images/desktop/screen-final.svg';
+import SlideDesktop1 from './images/desktop/slide-matches.svg';
+import SlideDesktop2 from './images/desktop/slide-voting.svg';
+import SlideDesktop3 from './images/desktop/slide-score.svg';
+import SlideDesktop4 from './images/desktop/slide-table.svg';
+import SlideDesktop5 from './images/desktop/slide-team.svg';
+import SlideDesktop6 from './images/desktop/slide-final.svg';
 
 const mobile = [SlideMobile1, SlideMobile2, SlideMobile3, SlideMobile4, SlideMobile5, SlideMobile6];
 
